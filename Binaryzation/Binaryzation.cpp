73d78d1315917c76d2f9d982eb7b8afa1d7b4963
Linengyao <1547784788@qq.com>
2020-02-28 14:46:43 +0800

    第三题

--- Binaryzation/Binaryzation.cpp
+++ Binaryzation/Binaryzation.cpp
@@ -1,11 +1,43 @@
-﻿// Binaryzation.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+﻿// PixelChange.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
 //
 
 #include <iostream>
+#include <opencv.hpp>
+#include "stdlib.h"
 
+using namespace cv;
 int main()
 {
-    std::cout << "Hello World!\n";
+	cv::Mat srcMat = imread("E:/桌面壁纸/1.jpg");
+	int height = srcMat.rows; //行数
+	int width = srcMat.cols;//每行元素数量
+	uchar threshold = 100;
+	for (int j = 0; j < height; j++)
+	{
+		for (int i = 0; i < width; i++)
+		{
+			//------开始处理每个像素---------
+			uchar average = (srcMat.at<Vec3b>(j, i)[0] +
+				srcMat.at<Vec3b>(j, i)[1] +
+				srcMat.at<Vec3b>(j, i)[2]) / 3;
+			if (average > threshold)
+			{
+				average = 255;
+			}
+			else
+			{
+				average = 0;
+			}
+			srcMat.at<Vec3b>(j, i)[0] = average;
+			srcMat.at<Vec3b>(j, i)[1] = average;
+			srcMat.at<Vec3b>(j, i)[2] = average;
+
+		}//单行处理结束
+	}
+	//std::cout << "Hello World!\n";
+	imshow("src", srcMat);
+	waitKey(0);
+	return 0;
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
